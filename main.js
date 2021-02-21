1c7efda4c8616587d7a24caefd08d289c05295fc
Paris Michelle <77037086+parismichelle@users.noreply.github.com>
2021-02-20 22:43:10 -0800

    create arrays

---
+++ main.js
@@ -0,0 +1,33 @@
+const beginning = [['You are as ', 'ADJECTIVE', ' as a ', 'FIRSTNOUN', ' ', 'PREPOSITION', ' ', 'LASTNOUN', '.'], 
+['You are more ', 'ADJECTIVE', ' than a ', 'FIRSTNOUN', ' ', 'PREPOSITION', ' ', 'LASTNOUN', '.'], 
+['You are ', 'ADJECTIVE', ' like a ', 'FIRSTNOUN', ' ', 'PREPOSITION', ' ', 'LASTNOUN', '.']];
+
+const adjective = ['beautiful', 'intelligent', 'smart', 'radiant', 'pretty', 'handsome', 'incredible', 'funny', 'magnificent', 
+'gorgeous', 'amazing', 'attractive', 'awesome', 'brilliant', 'charming', 'courageous', 'delightful', 'elegant', 'enchanting', 
+'exquisite', 'extraordinary', 'graceful', 'impressive', 'lovely', 'perfect', 'stunning', 'talented', 'trustworthy', 'strong', 'cute'];
+
+const firstNoun = ['manatee', 'sunflower', 'rose', 'daisy', 'dolphin', 'kitten', 'giraffe', 'diamond', 'flamingo', 'bobcat', 
+'lion', 'tiger', 'swan', 'cup of tea', 'mug of cocoa', 'cup of coffee', 'slice of cake', 'piece of pie', 'book', 
+'bouquet of flowers', 'bear', 'bird', 'owl', 'butterfly', 'tree', 'fox', 'bumble bee', 'duck', 'goat', 'unicorn', 'hippo', 
+'walrus', 'cupcake', 'mouse', 'rocket', 'palm tree', 'sailboat', 'bowl of icecream'];
+
+const preposition = ['in the', 'at', 'on a'];
+
+const lastNounIn = ['moonlight', 'sunlight', 'sunshine', 'ocean', 'wind', 'kitchen', 'garden', 'daytime', 'morning', 'summer',
+'snow', 'winter', 'autumn', 'spring', 'sky', 'forest', 'bathtub', 'library', 'park', 'bedroom', 'jungle', 'desert'];
+
+const lastNounAt = ['sunset', 'night', 'midday', 'bedtime', 'the beach', 'the lake', 'home', 'the library', 'a restaurant',
+'tea time', 'a sleepover', 'breakfast', 'midnight', 'dawn', 'noon', 'daybreak', 'high tide', 'a party', 'the ballet', 'a picnic'];
+
+const lastNounOn = ['Friday night', 'Sunday morning', 'Monday afternoon', 'summer evening', 'winter day', 'sunny morning', 
+'vacation', 'table', 'shelf', 'Hawaiian island', 'holiday', 'boat', 'mountain', 'weekend', 'beach']
+
+
+
+//You are as --- as a --- preposition ---.
+//You are more --- than a --- preposition ---. 
+//You are --- like a --- preposition ---. 
+
+/* console.log(process.argv);
+console.log(process.argv[2]);
+*/
